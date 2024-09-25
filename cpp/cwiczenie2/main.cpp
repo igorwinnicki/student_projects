263de6ea69f1c3f2f2434fe8047bc49347e647ed
igorwinnicki <146373497+igorwinnicki@users.noreply.github.com>
2024-09-25 07:58:03 +0200

    Update main.cpp

--- cpp/cwiczenie2/main.cpp
+++ cpp/cwiczenie2/main.cpp
@@ -56,6 +56,64 @@ int main(int argc, char **argv) {
     return RUN_ALL_TESTS();
 }
 
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// #include <ctime>
+// #include "gtest/gtest.h"
+// #include <chrono>
+// using namespace std;
+
+
+// void quick_sort(int *tab, int lewy, int prawy)
+// {
+// 	if(prawy <= lewy) return;
+	
+// 	int i = lewy - 1, j = prawy + 1, 
+// 	pivot = tab[(lewy+prawy)/2]; //wybieramy punkt odniesienia
+	
+// 	while(1)
+// 	{
+// 		while(pivot>tab[++i]);
+
+// 		while(pivot<tab[--j]);
+
+// 		if( i <= j)
+// 			swap(tab[i],tab[j]);
+// 		else
+// 			break;
+// 	}
+
+// 	if(j > lewy)
+// 	quick_sort(tab, lewy, j);
+// 	if(i < prawy)
+// 	quick_sort(tab, i, prawy);
+// }
+
+
+// // Test wydajności sortowania QuickSort
+// TEST(BenchmarkTest, QuickSortBenchmark) {
+//     std::vector<int> vec(10000); // Tworzymy wektor z 10000 losowymi liczbami
+//     std::srand(std::time(0)); // Inicjalizacja generatora liczb losowych
+//     std::generate(vec.begin(), vec.end(), std::rand);
+
+//     // Początek pomiaru czasu
+//     auto start = std::chrono::high_resolution_clock::now();
+
+//     quick_sort(tab,0, n-1); // Sortujemy wektor
+
+//     // Koniec pomiaru czasu
+//     auto end = std::chrono::high_resolution_clock::now();
+//     std::chrono::duration<double> elapsed_seconds = end - start;
+
+//     std::cout << "Czas sortowania: " << elapsed_seconds.count() << "s\n";
+// }
+
+// int main(int argc, char **argv) {
+//     ::testing::InitGoogleTest(&argc, argv);
+//     return RUN_ALL_TESTS();
+// }
+
 
 // int main()
 // {
