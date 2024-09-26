8c9783c68e71320bb651a9855b2f745ae35c877b
igorwinnicki <146373497+igorwinnicki@users.noreply.github.com>
2024-09-26 08:02:40 +0200

    Update main.cpp

--- cpp/cwiczenie2/main.cpp
+++ cpp/cwiczenie2/main.cpp
@@ -1,51 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
 #include <ctime>
 #include <chrono>
-#include "gtest/gtest.h"
 
-using namespace std;
+int partition(std::vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
 
-// QuickSort function
-void quick_sort(int *tab, int lewy, int prawy) {
-    if (prawy <= lewy) return;
-
-    int i = lewy - 1, j = prawy + 1;
-    int pivot = tab[(lewy + prawy) / 2]; // Choosing the pivot
-
-    while (true) {
-        while (pivot > tab[++i]);
-        while (pivot < tab[--j]);
-        
-        if (i <= j)
-            swap(tab[i], tab[j]);
-        else
-            break;
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            std::swap(arr[i], arr[j]);
+        }
     }
-
-    if (j > lewy)
-        quick_sort(tab, lewy, j);
-    if (i < prawy)
-        quick_sort(tab, i, prawy);
+    std::swap(arr[i + 1], arr[high]);
+    return (i + 1);
 }
 
-// Test for QuickSort performance
+void quick_sort(std::vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
+    }
+}
+
 TEST(BenchmarkTest, QuickSortBenchmark) {
-    const int n = 10000; // Size of the vector
-    std::vector<int> vec(n); // Create a vector with 10000 random numbers
-    std::srand(static_cast<unsigned int>(std::time(0))); // Initialize random number generator
+    std::vector<int> vec(10000);
+    std::srand(std::time(0));
     std::generate(vec.begin(), vec.end(), std::rand);
 
-    // Convert vector to array for quick_sort function
-    int* tab = vec.data();
-
-    // Start measuring time
     auto start = std::chrono::high_resolution_clock::now();
-
-    quick_sort(tab, 0, n - 1); // Sort the array
-
-    // End measuring time
+    quick_sort(vec, 0, vec.size() - 1);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
 
@@ -56,65 +44,6 @@ int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
-
-// #include <iostream>
-// #include <vector>
-// #include <algorithm>
-// #include <ctime>
-// #include "gtest/gtest.h"
-// #include <chrono>
-// using namespace std;
-
-
-// void quick_sort(int *tab, int lewy, int prawy)
-// {
-// 	if(prawy <= lewy) return;
-	
-// 	int i = lewy - 1, j = prawy + 1, 
-// 	pivot = tab[(lewy+prawy)/2]; //wybieramy punkt odniesienia
-	
-// 	while(1)
-// 	{
-// 		while(pivot>tab[++i]);
-
-// 		while(pivot<tab[--j]);
-
-// 		if( i <= j)
-// 			swap(tab[i],tab[j]);
-// 		else
-// 			break;
-// 	}
-
-// 	if(j > lewy)
-// 	quick_sort(tab, lewy, j);
-// 	if(i < prawy)
-// 	quick_sort(tab, i, prawy);
-// }
-
-
-// // Test wydajności sortowania QuickSort
-// TEST(BenchmarkTest, QuickSortBenchmark) {
-//     std::vector<int> vec(10000); // Tworzymy wektor z 10000 losowymi liczbami
-//     std::srand(std::time(0)); // Inicjalizacja generatora liczb losowych
-//     std::generate(vec.begin(), vec.end(), std::rand);
-
-//     // Początek pomiaru czasu
-//     auto start = std::chrono::high_resolution_clock::now();
-
-//     quick_sort(tab,0, n-1); // Sortujemy wektor
-
-//     // Koniec pomiaru czasu
-//     auto end = std::chrono::high_resolution_clock::now();
-//     std::chrono::duration<double> elapsed_seconds = end - start;
-
-//     std::cout << "Czas sortowania: " << elapsed_seconds.count() << "s\n";
-// }
-
-// int main(int argc, char **argv) {
-//     ::testing::InitGoogleTest(&argc, argv);
-//     return RUN_ALL_TESTS();
-// }
 
 
 // int main()
