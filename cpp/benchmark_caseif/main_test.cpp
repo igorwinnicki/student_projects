385ea733bfda6a32003f6cc46eea3398539131a0
igorwinnicki <146373497+igorwinnicki@users.noreply.github.com>
2024-10-03 07:43:39 +0200

    Update main_test.cpp

--- cpp/benchmark_caseif/main_test.cpp
+++ cpp/benchmark_caseif/main_test.cpp
@@ -4,22 +4,20 @@
 #include "gtest/gtest.h"
 #include <chrono>
 
-// Function to simulate if statements.
-void testIf(int value) {
-    if (value == 0) {}
-    else if (value == 1) {}
-    else if (value == 2) {}
-    else if (value == 3) {}
-    else if (value == 4) {}
-    else if (value == 5) {}
-    else if (value == 6) {}
-    else if (value == 7) {}
-    else if (value == 8) {}
-    else if (value == 9) {}
+void testIf(int value, const std::vector<int>& values) {
+    if (value == values[0]) {}
+    else if (value == values[1]) {}
+    else if (value == values[2]) {}
+    else if (value == values[3]) {}
+    else if (value == values[4]) {}
+    else if (value == values[5]) {}
+    else if (value == values[6]) {}
+    else if (value == values[7]) {}
+    else if (value == values[8]) {}
+    else if (value == values[9]) {}
 }
 
-// Function to simulate switch/case statements.
-void testSwitch(int value) {
+void testSwitch(int value, const std::vector<int>& values) {
     switch (value) {
         case 0: break;
         case 1: break;
@@ -37,11 +35,19 @@ void testSwitch(int value) {
 
 TEST(BenchmarkTest, IfStatementBenchmark) {
     std::srand(std::time(0));
+
+    // Pobranie wartości od użytkownika
+    std::vector<int> userValues(1);
+    std::cout << "Podaj 1 wartość do testu if: \n";
+    for (int& val : userValues) {
+        std::cin >> val;
+    }
+
     for (int i = 0; i < 10000; i++) {
         int randomValue = std::rand() % 10;
 
         auto start = std::chrono::high_resolution_clock::now();
-        testIf(randomValue);
+        testIf(randomValue, userValues);
         auto end = std::chrono::high_resolution_clock::now();
 
         std::chrono::duration<double> elapsed_seconds = end - start;
@@ -53,11 +59,19 @@ TEST(BenchmarkTest, IfStatementBenchmark) {
 
 TEST(BenchmarkTest, SwitchCaseBenchmark) {
     std::srand(std::time(0));
+
+    // Pobranie wartości od użytkownika
+    std::vector<int> userValues(1);
+    std::cout << "Podaj 1 wartość do testu switch/case: \n";
+    for (int& val : userValues) {
+        std::cin >> val;
+    }
+
     for (int i = 0; i < 10000; i++) {
         int randomValue = std::rand() % 10;
 
         auto start = std::chrono::high_resolution_clock::now();
-        testSwitch(randomValue);
+        testSwitch(randomValue, userValues);
         auto end = std::chrono::high_resolution_clock::now();
 
         std::chrono::duration<double> elapsed_seconds = end - start;
