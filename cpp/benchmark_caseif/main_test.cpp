05dc5807d175298106f259465cc321e76826f4f0
igorwinnicki <146373497+igorwinnicki@users.noreply.github.com>
2024-10-03 08:43:23 +0200

    Update main_test.cpp

--- cpp/benchmark_caseif/main_test.cpp
+++ cpp/benchmark_caseif/main_test.cpp
@@ -4,20 +4,22 @@
 #include "gtest/gtest.h"
 #include <chrono>
 
-void testIf(int value, const std::vector<int>& values) {
-    if (value == values[0]) {}
-    else if (value == values[1]) {}
-    else if (value == values[2]) {}
-    else if (value == values[3]) {}
-    else if (value == values[4]) {}
-    else if (value == values[5]) {}
-    else if (value == values[6]) {}
-    else if (value == values[7]) {}
-    else if (value == values[8]) {}
-    else if (value == values[9]) {}
+// Function to simulate if statements with a single user-provided value.
+void testIf(int value, int userValue) {
+    if (value == userValue) {}
+    else if (value == userValue + 1) {}
+    else if (value == userValue + 2) {}
+    else if (value == userValue + 3) {}
+    else if (value == userValue + 4) {}
+    else if (value == userValue + 5) {}
+    else if (value == userValue + 6) {}
+    else if (value == userValue + 7) {}
+    else if (value == userValue + 8) {}
+    else if (value == userValue + 9) {}
 }
 
-void testSwitch(int value, const std::vector<int>& values) {
+// Function to simulate switch/case statements with a single user-provided value.
+void testSwitch(int value, int userValue) {
     switch (value) {
         case 0: break;
         case 1: break;
@@ -36,12 +38,10 @@ void testSwitch(int value, const std::vector<int>& values) {
 TEST(BenchmarkTest, IfStatementBenchmark) {
     std::srand(std::time(0));
 
-    // Pobranie wartości od użytkownika
-    std::vector<int> userValues(10);
-    std::cout << "Podaj 10 wartości do testu if: \n";
-    for (int& val : userValues) {
-        std::cin >> val;
-    }
+    // Pobranie jednej wartości od użytkownika
+    int userValue;
+    std::cout << "Podaj jedną wartość do testu if: ";
+    std::cin >> userValue;
 
     // Zmienna do sumowania czasu
     std::chrono::duration<double> total_time_if(0);
@@ -50,7 +50,7 @@ TEST(BenchmarkTest, IfStatementBenchmark) {
         int randomValue = std::rand() % 10;
 
         auto start = std::chrono::high_resolution_clock::now();
-        testIf(randomValue, userValues);
+        testIf(randomValue, userValue);
         auto end = std::chrono::high_resolution_clock::now();
 
         std::chrono::duration<double> elapsed_seconds = end - start;
@@ -64,12 +64,10 @@ TEST(BenchmarkTest, IfStatementBenchmark) {
 TEST(BenchmarkTest, SwitchCaseBenchmark) {
     std::srand(std::time(0));
 
-    // Pobranie wartości od użytkownika
-    std::vector<int> userValues(10);
-    std::cout << "Podaj 10 wartości do testu switch/case: \n";
-    for (int& val : userValues) {
-        std::cin >> val;
-    }
+    // Pobranie jednej wartości od użytkownika
+    int userValue;
+    std::cout << "Podaj jedną wartość do testu switch/case: ";
+    std::cin >> userValue;
 
     // Zmienna do sumowania czasu
     std::chrono::duration<double> total_time_switch(0);
@@ -78,7 +76,7 @@ TEST(BenchmarkTest, SwitchCaseBenchmark) {
         int randomValue = std::rand() % 10;
 
         auto start = std::chrono::high_resolution_clock::now();
-        testSwitch(randomValue, userValues);
+        testSwitch(randomValue, userValue);
         auto end = std::chrono::high_resolution_clock::now();
 
         std::chrono::duration<double> elapsed_seconds = end - start;
