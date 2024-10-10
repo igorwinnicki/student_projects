8a290b1f9accada09e602f12fe4c88c335904337
igorwinnicki <146373497+igorwinnicki@users.noreply.github.com>
2024-10-10 07:51:50 +0200

    Update functions.cpp

--- cpp/fizzbuzz/functions.cpp
+++ cpp/fizzbuzz/functions.cpp
@@ -1,4 +1,13 @@
+#include <string>
 
-int suma(int a, int b) {
-    return a + b;
+std::string fizzBuzz(int number) {
+    if (number % 3 == 0 && number % 5 == 0) {
+        return "FizzBuzz";
+    } else if (number % 3 == 0) {
+        return "Fizz";
+    } else if (number % 5 == 0) {
+        return "Buzz";
+    } else {
+        return std::to_string(number);
+    }
 }
