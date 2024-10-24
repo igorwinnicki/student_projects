419097bdd9c07933d484309b8e71104c256b8db4
igorwinnicki <146373497+igorwinnicki@users.noreply.github.com>
2024-10-24 08:11:02 +0200

    Update functions.cpp

--- cpp/schody_benchmark/functions.cpp
+++ cpp/schody_benchmark/functions.cpp
@@ -1,13 +1,19 @@
-#include <string>
+#include <iostream>
+#include <cmath>
 
-std::string fizzBuzz(int number) {
-    if (number % 3 == 0 && number % 5 == 0) {
-        return "FizzBuzz";
-    } else if (number % 3 == 0) {
-        return "Fizz";
-    } else if (number % 5 == 0) {
-        return "Buzz";
-    } else {
-        return std::to_string(number);
-    }
+// Funkcja do obliczenia liczby stopni
+int obliczIloscStopni(double wysokosc, double dlugosc) {
+    const double minWysokosc = 15.0;
+    const double minDlugosc = 28.0;
+
+    int iloscStopniWysokosc = std::floor(wysokosc / minWysokosc);
+    int iloscStopniDlugosc = std::floor(dlugosc / minDlugosc);
+
+    return std::min(iloscStopniWysokosc, iloscStopniDlugosc);
+}
+
+// Funkcja do obliczenia wymiarów stopnia
+void obliczWymiaryStopnia(double dlugosc, int iloscStopni, double& dlugoscStopnia, double& szerokoscStopnia) {
+    dlugoscStopnia = dlugosc / iloscStopni;
+    szerokoscStopnia = 15.0; // Szerokość stopnia ustalamy na stałą wartość
 }
