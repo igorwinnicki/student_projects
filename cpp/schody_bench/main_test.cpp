9c6258476c45096cbc8b8fc4ee0454335b90275b
igorwinnicki <146373497+igorwinnicki@users.noreply.github.com>
2024-10-24 08:32:29 +0200

    Update main_test.cpp

--- cpp/schody_bench/main_test.cpp
+++ cpp/schody_bench/main_test.cpp
@@ -1,47 +1,27 @@
 #include <gtest/gtest.h>
 #include "functions.cpp"
 
-TEST(FizzBuzzTest, DivisibleBy3) {
-    EXPECT_EQ(fizzBuzz(3), "Fizz");
-    EXPECT_EQ(fizzBuzz(6), "Fizz");
-    EXPECT_EQ(fizzBuzz(9), "Fizz");
+// Prototypy funkcji
+double obliczIloscStopni(double wysokosc, double dlugosc);
+void obliczWymiaryStopnia(double dlugosc, int iloscStopni, double& dlugoscStopnia, double& szerokoscStopnia);
+
+// Testy jednostkowe
+TEST(SchodoweTests, TestIloscStopni) {
+    EXPECT_EQ(obliczIloscStopni(60.0, 100.0), 4);
+    EXPECT_EQ(obliczIloscStopni(30.0, 60.0), 2);
+    EXPECT_EQ(obliczIloscStopni(15.0, 30.0), 1);
 }
 
-TEST(FizzBuzzTest, DivisibleBy5) {
-    EXPECT_EQ(fizzBuzz(5), "Buzz");
-    EXPECT_EQ(fizzBuzz(10), "Buzz");
-    EXPECT_EQ(fizzBuzz(20), "Buzz");
+TEST(SchodoweTests, TestWymiaryStopnia) {
+    double dlugoscStopnia, szerokoscStopnia;
+    obliczWymiaryStopnia(100.0, 4, dlugoscStopnia, szerokoscStopnia);
+    
+    EXPECT_DOUBLE_EQ(dlugoscStopnia, 25.0);
+    EXPECT_DOUBLE_EQ(szerokoscStopnia, 15.0);
 }
 
-TEST(FizzBuzzTest, DivisibleBy3And5) {
-    EXPECT_EQ(fizzBuzz(15), "FizzBuzz");
-    EXPECT_EQ(fizzBuzz(30), "FizzBuzz");
-    EXPECT_EQ(fizzBuzz(45), "FizzBuzz");
-}
-
-TEST(FizzBuzzTest, NotDivisibleBy3Or5) {
-    EXPECT_EQ(fizzBuzz(1), "1");
-    EXPECT_EQ(fizzBuzz(2), "2");
-    EXPECT_EQ(fizzBuzz(4), "4");
-}
-
-TEST(FizzBuzzTest, AutomaticTest) {
-    for (int i = 1; i <= 100; ++i) {
-        std::string expected;
-        if (i % 3 == 0 && i % 5 == 0) {
-            expected = "FizzBuzz";
-        } else if (i % 3 == 0) {
-            expected = "Fizz";
-        } else if (i % 5 == 0) {
-            expected = "Buzz";
-        } else {
-            expected = std::to_string(i);
-        }
-        EXPECT_EQ(fizzBuzz(i), expected) << "Failed for number: " << i;
-    }
-}
-
+// Uruchomienie testów
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
+    testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
