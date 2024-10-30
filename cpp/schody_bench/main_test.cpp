e2b7fecdd052ca6cd37c0aa1c12a0ab80b3c9bd8
igorwinnicki <146373497+igorwinnicki@users.noreply.github.com>
2024-10-30 08:22:34 +0100

    Update main_test.cpp

--- cpp/schody_bench/main_test.cpp
+++ cpp/schody_bench/main_test.cpp
@@ -1,9 +1,32 @@
-#include <gtest/gtest.h>
-#include "functions.cpp"
+// #include <gtest/gtest.h>
+// #include "functions.cpp"
 
-// Prototypy funkcji
-double obliczIloscStopni(double wysokosc, double dlugosc);
-void obliczWymiaryStopnia(double dlugosc, int iloscStopni, double& dlugoscStopnia, double& szerokoscStopnia);
+// // Prototypy funkcji
+// double obliczIloscStopni(double wysokosc, double dlugosc);
+// void obliczWymiaryStopnia(double dlugosc, int iloscStopni, double& dlugoscStopnia, double& szerokoscStopnia);
+
+// // Testy jednostkowe
+// TEST(SchodoweTests, TestIloscStopni) {
+//     EXPECT_EQ(obliczIloscStopni(300, 400), 14);
+//     EXPECT_EQ(obliczIloscStopni(30.0, 60.0), 2);
+//     EXPECT_EQ(obliczIloscStopni(15.0, 30.0), 1);
+// }
+
+// TEST(SchodoweTests, TestWymiaryStopnia) {
+//     double dlugoscStopnia, szerokoscStopnia;
+//     obliczWymiaryStopnia(100.0, 4, dlugoscStopnia, szerokoscStopnia);
+    
+//     EXPECT_DOUBLE_EQ(dlugoscStopnia, 25.0);
+//     EXPECT_DOUBLE_EQ(szerokoscStopnia, 15.0);
+// }
+
+// // Uruchomienie testów
+// int main(int argc, char **argv) {
+//     testing::InitGoogleTest(&argc, argv);
+//     return RUN_ALL_TESTS();
+// }
+#include <gtest/gtest.h>
+#include "functions.h"
 
 // Testy jednostkowe
 TEST(SchodoweTests, TestIloscStopni) {
