61539ea03bd756e298a0f2873f0040d8f2d74e8a
igorwinnicki <146373497+igorwinnicki@users.noreply.github.com>
2024-10-30 08:23:24 +0100

    Create functions.h

---
+++ cpp/schody_bench/functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+// Deklaracje funkcji
+double obliczIloscStopni(double wysokosc, double dlugosc);
+void obliczWymiaryStopnia(double dlugosc, int iloscStopni, double& dlugoscStopnia, double& szerokoscStopnia);
+void wprowadzenieWymiarow(double& wysokosc, double& dlugosc);
+
+#endif // FUNCTIONS_H
