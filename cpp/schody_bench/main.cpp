b04a53e11e409322d48481c53bf6521cad2ab3ac
igorwinnicki <146373497+igorwinnicki@users.noreply.github.com>
2024-11-20 07:55:56 +0100

    Update main.cpp

--- cpp/schody_bench/main.cpp
+++ cpp/schody_bench/main.cpp
@@ -1,38 +1,3 @@
-// #include <iostream>
-// #include "functions.cpp"
-
-// int main() {
-//     double wysokosc = 0.0, dlugosc = 0.0;
-
-//     // Pobranie wymiarów od użytkownika
-//     wprowadzenieWymiarow(wysokosc, dlugosc);
-
-//     // Sprawdzenie, czy wprowadzono prawidłowe dane
-//     if (wysokosc <= 0 || dlugosc <= 0) {
-//         std::cerr << "Nieprawidłowe wymiary, program zakończy działanie." << std::endl;
-//         return 1;
-//     }
-
-//     // Obliczenie liczby stopni
-//     double iloscStopni = obliczIloscStopni(wysokosc, dlugosc);
-    
-//     // Sprawdzenie, czy obliczenia były możliwe
-//     if (iloscStopni > 0) {
-//         double dlugoscStopnia = 0.0, wysokoscStopnia = 0.0;
-
-//         // Obliczenie wymiarów stopnia
-//         obliczWymiaryStopnia(dlugosc, iloscStopni, dlugoscStopnia, wysokoscStopnia);
-
-//         // Wyświetlenie wyników
-//         std::cout << "Liczba stopni: " << iloscStopni << std::endl;
-//         std::cout << "Długość pojedynczego stopnia: " << dlugoscStopnia << " cm" << std::endl;
-//         std::cout << "Wysokość pojedynczego stopnia: " << wysokoscStopnia << " cm" << std::endl;
-//     } else {
-//         std::cerr << "Nie można obliczyć schodów z podanych wymiarów." << std::endl;
-//     }
-
-//     return 0;
-// }
 #include <iostream>
 #include "functions.cpp"
 
